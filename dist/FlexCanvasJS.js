1caedae061ae98efec42727ab383971ac020271d
Nathan Nelson <33074286+nanelson@users.noreply.github.com>
2018-10-30 16:58:28 -0700

    Update dist

--- dist/FlexCanvasJS.js
+++ dist/FlexCanvasJS.js
@@ -20173,7 +20173,7 @@ DropdownElement.prototype.setSelectedItem =
 	function (item)
 	{
 		var index = this._listCollection.getItemIndex(item);
-		this._setSelectedIndex(index);
+		this.setSelectedIndex(index);
 	};
 	
 /**
