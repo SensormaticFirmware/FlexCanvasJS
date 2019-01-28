b85ecd560c4a28c1c658a22fe6ad8ceef2e30529
Nathan Nelson <33074286+nanelson@users.noreply.github.com>
2019-01-28 14:59:24 -0800

    Update dist

--- dist/FlexCanvasJS.js
+++ dist/FlexCanvasJS.js
@@ -20283,6 +20283,9 @@ function CanvasManager()
 	//Now call base
 	CanvasManager.base.prototype.constructor.call(this);
 
+	this._alertQueue = [];
+	this._alertModal = null;
+	
 	this._cursorContainer = new CanvasElement();
 	this._cursorContainer.setStyle("MouseEnabled", false);
 	this._addOverlayChild(this._cursorContainer);	
@@ -20719,12 +20722,36 @@ CanvasManager._StyleTypes = Object.create(null);
  */
 CanvasManager._StyleTypes.ShowRedrawRegion = 								StyleableBase.EStyleType.NORMAL;		
 
+/**
+ * @style AlertModalClass CanvasElement
+ * 
+ * CanvasElement constructor to be used for the alert overlay. Defaults to CanvasElement.
+ * This may be set to null if you do not wish to block interactivity with the underlying UI when
+ * and alert is triggered. 
+ */
+CanvasManager._StyleTypes.AlertModalClass = 								StyleableBase.EStyleType.NORMAL;		
+
+/**
+ * @style AlertModalStyle StyleDefinition
+ * 
+ * The StyleDefinition or [StyleDefinition] array to apply to the alert modal overlay.
+ * Default definition sets BackgroundFill to "rgba(180,180,180,.4)".
+ * If styled to be transparent, the modal element will still block user interactivity.
+ */
+CanvasManager._StyleTypes.AlertModalStyle = 								StyleableBase.EStyleType.SUBSTYLE;		
+
 
 /////////////Default Styles///////////////////////////////
 
+CanvasManager.AlertModalStyle = new StyleDefinition();
+CanvasManager.AlertModalStyle.setStyle("BackgroundFill", 					"rgba(180,180,180,.4)");
+
+
 CanvasManager.StyleDefault = new StyleDefinition();
 
-CanvasManager.StyleDefault.setStyle("ShowRedrawRegion", 					false);		// true || false
+CanvasManager.StyleDefault.setStyle("ShowRedrawRegion", 					false);							// true || false
+CanvasManager.StyleDefault.setStyle("AlertModalClass", 						CanvasElement);
+CanvasManager.StyleDefault.setStyle("AlertModalStyle", 						CanvasManager.AlertModalStyle);	
 
 
 
@@ -20826,6 +20853,74 @@ CanvasManager.prototype.getCanvas =
 		return this._canvas;
 	};
 
+/**
+ * @function addAlert
+ * Adds an element to be displayed as an alert. Only one alert may
+ * be displayed at a time, subsequent will be queued and displayed in the
+ * order they have been queued as previous alerts are removed. 
+ * If an AlertModalClass style is set (defaults to CanvasElement), the modal 
+ * element will be placed covering all content under the alert element.
+ * 
+ * @returns CanvasElement
+ * The element supplied to addAlert().
+ */		
+CanvasManager.prototype.addAlert = 
+	function (element)
+	{
+		this._alertQueue.push(element);
+		
+		if (this._alertQueue.length == 1)
+		{
+			if (this._alertModal != null)
+				this.addElement(this._alertModal);
+			
+			this.addElement(element);
+		}
+		
+		return element;
+	};
+	
+/**
+ * @function removeAlert
+ * Removes an alert element. If this is the last alert, the modal
+ * element will also be removed.
+ * 
+ * @param element CanvasElement
+ * The element to remove from the alert list.
+ * 
+ * @returns CanvasElement
+ * The element supplied to removeAlert() or null if invalid.
+ */		
+CanvasManager.prototype.removeAlert = 
+	function (element)
+	{
+		var index = this._alertQueue.indexOf(element);
+		if (index == -1)
+			return null;
+	
+		this._alertQueue.splice(index, 1);
+		
+		//We removed the current alert
+		if (index == 0)
+		{
+			this.removeElement(element);
+			
+			if (this._alertModal != null)
+			{
+				if (this._alertQueue.length == 0)
+					this.removeElement(this._alertModal); //No more alerts, remove modal
+				else
+					this.setElementIndex(this._alertModal, this.getNumElements()); //more alerts in queue, push modal to end
+			}
+			
+			//Add next alert in queue
+			if (this._alertQueue.length > 0)
+				this.addElement(this._alertQueue[0]);
+		}			
+		
+		return element;
+	};
+	
 /**
  * @function setLocale
  * Sets the locale to be used when using localized strings. The actual
@@ -21499,14 +21594,52 @@ CanvasManager.prototype._setDraggingElement =
 		this._draggingOffsetY = offsetY;
 	};
 	
-//@Override	
-CanvasManager.prototype._doLayout = 
-function (paddingMetrics)
-{
-	CanvasManager.base.prototype._doLayout.call(this, paddingMetrics);
+//@override	
+CanvasManager.prototype._doStylesUpdated = 
+	function (stylesMap)
+	{
+		CanvasManager.base.prototype._doStylesUpdated.call(this, stylesMap);
 	
-	this._cursorContainer._setActualSize(this._width, this._height);
-};	
+		var newModal = false;
+		if ("AlertModalClass" in stylesMap)
+		{
+			var modalClass = this.getStyle("AlertModalClass");
+			
+			//Destroy if class is null or does not match existing
+			if ((modalClass == null && this._alertModal != null) ||
+				this._alertModal != null && this._alertModal.constructor != modalClass)
+			{
+				//Try to remove it (it may not be attached)
+				this.removeElementAt(this.getElementIndex(this._alertModal));
+				this._alertModal = null;
+			}
+			
+			//Create
+			if (modalClass != null && this._alertModal == null)
+			{
+				newModal = true;
+				this._alertModal = new (modalClass)();
+				this._alertModal.setStyle("IncludeInLayout", false); //Always false, we do this manually to prevent layout styles
+				
+				//Add behind first alert position
+				if (this._alertQueue.length > 0)
+					this.addElementAt(this._alertModal, this.getElementIndex(this._alertQueue[0]));
+			}
+		}
+		
+		if (this._alertModal != null && ("AlertModalStyle" in stylesMap || newModal == true))
+			this._applySubStylesToElement("AlertModalStyle", this._alertModal);
+	};
+	
+//@override	
+CanvasManager.prototype._doLayout = 
+	function (paddingMetrics)
+	{
+		CanvasManager.base.prototype._doLayout.call(this, paddingMetrics);
+		
+		this._alertModal._setActualSize(this._width, this._height);
+		this._cursorContainer._setActualSize(this._width, this._height);
+	};	
 
 
 
@@ -26378,4 +26511,364 @@ CheckboxElement.prototype._doStylesUpdated =
 	};	
 	
 	
+
+
+/**
+ * @depends ListContainerElement.js
+ * @depends ButtonElement.js
+ */
+
+//////////////////////////////////////////////////////////////
+//////////////////AlertElement////////////////////////////////
+
+/**
+ * @class AlertElement
+ * @inherits ListContainerElement
+ * 
+ * AlertElement is a class used to render pop up alerts.  Any element 
+ * may be used as an alert. This class is a helper that implements
+ * common features, styles, and functionality for basic alerts. 
+ * 
+ * See CanvasManager addAlert() and removeAlert() for more info.
+ * 
+ * @constructor AlertElement 
+ * Creates new AlertElement instance.
+ */
+
+function AlertElement() //extends ListContainerElement
+{
+	AlertElement.base.prototype.constructor.call(this);
+	
+		this._labelTitle = new LabelElement();
+		this._labelTitle.setStyle("PercentWidth", 100);
+		
+		this._contentContainer = new ListContainerElement();
+		this._contentContainer.setStyle("PercentWidth", 100);
+		this._contentContainer.setStyle("PercentHeight", 100);
+	
+			this._textContent = new TextElement();
+			this._textContent.setStyle("PercentWidth", 100);
+			this._textContent.setStyle("PercentHeight", 100);
+			
+			this._buttonContainer = new ListContainerElement();
+			this._buttonContainer.setStyle("PercentWidth", 100);
+			
+		this._contentContainer.addElement(this._textContent);
+		this._contentContainer.addElement(this._buttonContainer);
+		
+	this.addElement(this._labelTitle);
+	this.addElement(this._contentContainer);
+	
+	
+	///////Event handlers////////////
+	
+	var _self = this;
+	
+	//Private event handlers, proxy to prototype.
+	
+	this._onAlertClickInstance = 
+		function (mouseEvent)
+		{
+			_self._onAlertClick(mouseEvent);
+		}
+	
+	this.addEventListener("click", this._onAlertClickInstance);
+	
+	
+	/////////////////////////////////
+	
+	this._selectionCallback = null;
+	this._buttonTextArray = [];
+}
+
+//Inherit from ListContainerElement
+AlertElement.prototype = Object.create(ListContainerElement.prototype);
+AlertElement.prototype.constructor = AlertElement;
+AlertElement.base = ListContainerElement;
+
+
+/////////////Style Types///////////////////////////////
+
+AlertElement._StyleTypes = Object.create(null);
+
+/**
+ * @style TitleLabelStyle StyleDefinition
+ * 
+ * The StyleDefinition or [StyleDefinition] array to apply to the title LabelElement.
+ */
+AlertElement._StyleTypes.TitleLabelStyle = 									StyleableBase.EStyleType.SUBSTYLE;
+
+/**
+ * @style ContentListContainerStyle StyleDefinition
+ * 
+ * The StyleDefinition or [StyleDefinition] array to apply to content ListContainer.
+ * This container parents the content text and button list.
+ */
+AlertElement._StyleTypes.ContentListContainerStyle = 						StyleableBase.EStyleType.SUBSTYLE;
+
+/**
+ * @style ButtonListContainerStyle StyleDefinition
+ * 
+ * The StyleDefinition or [StyleDefinition] array to apply to button ListContainer.
+ * This container parents the alert selection buttons.
+ */
+AlertElement._StyleTypes.ButtonListContainerStyle = 						StyleableBase.EStyleType.SUBSTYLE;
+
+/**
+ * @style ContentTextStyle StyleDefinition
+ * 
+ * The StyleDefinition or [StyleDefinition] array to apply to the content TextElement.
+ */
+AlertElement._StyleTypes.ContentTextStyle = 								StyleableBase.EStyleType.SUBSTYLE;
+
+/**
+ * @style ButtonClass CanvasElement
+ * 
+ * The CanvasElement or subclass constructor type used to generate the alert selection buttons.
+ * Default value is ButtonElement. Alert will set the "Text" style and the _listData on this element 
+ * via the text supplied by the array passed to setButtons().
+ */
+AlertElement._StyleTypes.ButtonClass = 										StyleableBase.EStyleType.NORMAL;
+
+/**
+ * @style ButtonStyle StyleDefinition
+ * 
+ * The StyleDefinition or [StyleDefinition] array to apply to the alert selection buttons.
+ */
+AlertElement._StyleTypes.ButtonStyle = 										StyleableBase.EStyleType.SUBSTYLE;
+
+
+////////////Default Styles///////////////////////////
+
+AlertElement.TitleLabelStyle = new StyleDefinition();
+AlertElement.TitleLabelStyle.setStyle("TextStyle", 							"bold");
+AlertElement.TitleLabelStyle.setStyle("Padding", 							5);
+AlertElement.TitleLabelStyle.setStyle("PaddingTop", 						10);
+AlertElement.TitleLabelStyle.setStyle("TextHorizontalAlign", 				"center");
+
+AlertElement.ContentListContainerStyle = new StyleDefinition();
+AlertElement.ContentListContainerStyle.setStyle("Padding", 					5);
+AlertElement.ContentListContainerStyle.setStyle("LayoutGap", 				10);
+
+AlertElement.ContentTextStyle = new StyleDefinition();
+AlertElement.ContentTextStyle.setStyle("TextHorizontalAlign", 				"center");
+
+AlertElement.ButtonListContainerStyle = new StyleDefinition();
+AlertElement.ButtonListContainerStyle.setStyle("LayoutHorizontalAlign", 	"center");
+AlertElement.ButtonListContainerStyle.setStyle("LayoutDirection", 			"horizontal");
+AlertElement.ButtonListContainerStyle.setStyle("LayoutGap", 				10);
+AlertElement.ButtonListContainerStyle.setStyle("Padding", 					5);
+
+AlertElement.ButtonStyle = new StyleDefinition();
+AlertElement.ButtonStyle.setStyle("MinWidth", 70);
+
+AlertElement.StyleDefault = new StyleDefinition();
+AlertElement.StyleDefault.setStyle("BorderType", 							"solid");
+AlertElement.StyleDefault.setStyle("BorderThickness", 						1);
+AlertElement.StyleDefault.setStyle("BackgroundFill", 						"#FFFFFF");
+AlertElement.StyleDefault.setStyle("ShadowSize", 							2);
+AlertElement.StyleDefault.setStyle("ShadowOffsetX", 						1);
+AlertElement.StyleDefault.setStyle("ShadowOffsetY", 						1);
+AlertElement.StyleDefault.setStyle("HorizontalCenter", 						0);
+AlertElement.StyleDefault.setStyle("VerticalCenter", 						0);
+AlertElement.StyleDefault.setStyle("MinWidth", 								300);
+AlertElement.StyleDefault.setStyle("MinHeight", 							100);
+AlertElement.StyleDefault.setStyle("ButtonClass", 							ButtonElement);
+AlertElement.StyleDefault.setStyle("ButtonStyle", 							AlertElement.ButtonStyle);
+AlertElement.StyleDefault.setStyle("TitleLabelStyle", 						AlertElement.TitleLabelStyle);
+AlertElement.StyleDefault.setStyle("ContentListContainerStyle", 			AlertElement.ContentListContainerStyle);
+AlertElement.StyleDefault.setStyle("ContentTextStyle", 						AlertElement.ContentTextStyle);
+AlertElement.StyleDefault.setStyle("ButtonListContainerStyle", 				AlertElement.ButtonListContainerStyle);
+
+/**
+ * @function createAlert
+ * @static
+ * Helper function for automatically creating, showing, and closing an alert.
+ * 
+ * @param canvasManager CanvasManager
+ * The CanvasManager instance the alert should be shown.
+ * 
+ * @param titleText String
+ * Text that should be applied to the title LabelElement
+ * 
+ * @param contentText String
+ * Text that should be applied to the content TextElement
+ * 
+ * @param buttonTextArray Array
+ * Array of String used to generate the alert selection buttons and text.
+ * 
+ * @param styleDefinitions StyleDefinition
+ * StyleDefinition or [StyleDefinition] array to apply to the alert instance.
+ * 
+ * @param alertClosedCallback Function
+ * Function to be called when the alert is closed via one of the selection buttons being clicked.
+ * Signature: function onButtonClicked(index, text)
+ * 
+ * @returns AlertElement
+ * The newly created alert instance
+ */
+AlertElement.createAlert = 
+	function (canvasManager, titleText, contentText, buttonTextArray, styleDefinitions, alertClosedCallback)
+	{
+		var alert = new AlertElement();
+		alert.setStyleDefinitions(styleDefinitions);
+		alert.setTitleText(titleText);
+		alert.setContentText(contentText);
+		alert.setButtons(buttonTextArray);
+		
+		alert.setSelectionCallback(
+			function(index, text)
+			{
+				canvasManager.removeAlert(alert);
+				
+				if (alertClosedCallback != null)
+					alertCloseCallback(index, text);
+			});
+		
+		canvasManager.addAlert(alert);
+		return alert;
+	};
+	
+
+///////////Public/////////////////////////////////////
+
+/**
+ * @function setTitleText
+ * Sets the text to apply to the alert title LabelElement
+ * 
+ * @param text String
+ * The string use as the title text.
+ */
+AlertElement.prototype.setTitleText = 
+	function (text)
+	{
+		this._labelTitle.setStyle("Text", text);
+	};
+	
+/**
+ * @function setContentText
+ * Sets the text to apply to the alert content TextElement
+ * 
+ * @param text String
+ * The string use as the content text.
+ */	
+AlertElement.prototype.setContentText = 
+	function (text)
+	{
+		this._textContent.setStyle("Text", text);
+	};
+	
+/**
+ * @function setButtons
+ * Sets the array of text to used to generate the alert's selection buttons.
+ * 
+ * @param buttonTextArray Array
+ * Array of Strings used to generate buttons and apply text. 
+ */		
+AlertElement.prototype.setButtons = 
+	function (buttonTextArray)
+	{
+		//Use emtpy array rather than null
+		if (buttonTextArray == null)
+			buttonTextArray = [];
+	
+		//Copy the buttons in case styles change later
+		if (this._buttonTextArray != buttonTextArray)
+			this._buttonTextArray = buttonTextArray.slice();
+	
+		var i;
+		var button;
+		var buttonClass = this.getStyle("ButtonClass");
+		
+		//Handle button class change - happens when called due to style change (purge all buttons)
+		if (buttonClass == null || 
+			(this._buttonContainer.getNumElements() > 0 && this._buttonContainer.getElementAt(0).constructor != buttonClass))
+		{
+			while (this._buttonContainer.getNumElements() > 0)
+				this._buttonContainer.removeElementAt(0);
+		}
+		
+		//Purge excess buttons
+		while (this._buttonContainer.getNumElements() > buttonTextArray.length)
+			this._buttonContainer.removeElementAt(this._buttonContainer.getNumElements() - 1);
+		
+		//Create new buttons & set text
+		if (buttonClass != null)
+		{
+			for (i = 0; i < buttonTextArray.length; i++)
+			{
+				button = this._buttonContainer.getElementAt(i);
+				
+				if (button == null)
+				{
+					button = new (buttonClass)();
+					this._applySubStylesToElement("ButtonStyle", button);
+					this._buttonContainer.addElementAt(button, i);
+				}
+				
+				button.setStyle("Text", buttonTextArray[i]);
+				button._setListData(new DataListData(this, i), buttonTextArray[i]);
+			}
+		}
+	};
+
+/**
+ * @function setSelectionCallback
+ * Sets the callback to be called when an alert button is clicked.
+ * 
+ * @param callbackFunction Function
+ * Function to call when an alert button is clicked.
+ * Signature: function onAlertButtonClicked(index, text) 
+ */		
+AlertElement.prototype.setSelectionCallback = 
+	function (callbackFunction)
+	{
+		this._selectionCallback = callbackFunction
+	};
+
+///////////Internal/////////////////////////////////////
+
+//@override
+AlertElement.prototype._doStylesUpdated =
+	function (stylesMap)
+	{
+		AlertElement.base.prototype._doStylesUpdated.call(this, stylesMap);
+	
+		////Update Substyles/////////
+		
+		if ("TitleLabelStyle" in stylesMap)
+			this._applySubStylesToElement("TitleLabelStyle", this._labelTitle);
+		
+		if ("ContentListContainerStyle" in stylesMap)
+			this._applySubStylesToElement("ContentListContainerStyle", this._contentContainer);
+		
+		if ("ButtonListContainerStyle" in stylesMap)
+			this._applySubStylesToElement("ButtonListContainerStyle", this._buttonContainer);
+		
+		if ("ContentTextStyle" in stylesMap)
+			this._applySubStylesToElement("ContentTextStyle", this._textContent);
+		
+		if ("ButtonStyle" in stylesMap || "ButtonClass" in stylesMap)
+			this.setButtons(this._buttonTextArray);
+	};	
+
+AlertElement.prototype._onAlertClick = 
+	function (mouseEvent)
+	{
+		//Bail if no callback
+		if (this._selectionCallback == null)
+			return;
+		
+		var buttonIndex = this._buttonContainer.getElementIndex(mouseEvent.getTarget());
+		
+		//Bail if it wasn't one of the buttons that fired the event
+		if (buttonIndex == -1)
+			return;
+		
+		//Do callback
+		this._selectionCallback(buttonIndex, this._buttonTextArray[buttonIndex]);
+	};
+
+
+
 
